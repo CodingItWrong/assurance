9269b6a725920d6b334a40bab2e13a045eb47287
Josh Justice <josh.justice@accesscfa.com>
2020-01-08 08:41:00 -0500

    Add Google Analytics

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,11 @@
 module.exports = {
   title: 'Assurance',
+  plugins: [
+    [
+      '@vuepress/google-analytics',
+      { ga: 'UA-155752933-1' },
+    ],
+  ],
   themeConfig: {
     search: false,
     sidebar: [
